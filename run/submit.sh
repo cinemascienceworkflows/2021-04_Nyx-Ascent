6f5f33015559ef885890283d3dcd3f55a84c5fda
David Rogers <dhr@lanl.gov>
2021-08-10 17:53:03 -0400

    submit on existing files (non-Pantheon)

--- run/submit.sh
+++ run/submit.sh
@@ -3,7 +3,7 @@
 # Begin LSF Directives
 #BSUB -P <compute_allocation>
 #BSUB -J <pantheon_workflow_jid> 
-#BSUB -W 0:03
+#BSUB -W 0:10
 #BSUB -nnodes 1
 #BSUB -alloc_flags "gpumps"
 #BSUB -o Nyx64_Test.%J
@@ -12,6 +12,6 @@
 date
 
 module load gcc/6.4.0 
-module load cuda/10.2.89
+module load cuda/10.1.168
 
-jsrun -n 6 -a 1 -g 1 -c 7 <pantheon_run_dir>/Nyx3d.gnu.TPROF.MPI.CUDA.ex inputs_nohydro.64.cuda  insitu.int=1 max_step=3 amr.max_grid_size=32 amr.regrid_on_restart=1
+jsrun -n 6 -a 1 -g 1 -c 7 <pantheon_run_dir>/Nyx3d.gnu.TPROF.MTMPI.CUDA.ex inputs.rt  insitu.int=1 max_step=3 amr.max_grid_size=32 amr.regrid_on_restart=1
